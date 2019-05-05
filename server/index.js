2dd263b5919d5b38ef4322a7dc968d95f14d06b3
fradleybrancis <fradleybrancis@gmail.com>
2019-05-04 17:11:11 -0700

    delete unnecessary components, update server routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers');
 
 var items = require('../database-mongo');
 
@@ -7,15 +8,7 @@ var app = express();
 
 app.use(express.static(__dirname + '/../react-client/dist'));
 
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
-});
+app.get('/items', controllers.getAll)
 
 app.listen(3000, function() {
   console.log('listening on port 3000!');
