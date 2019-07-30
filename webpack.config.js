cbaa80b4203fc4959cb3d048011534fb84c235ac
fradleybrancis <fradleybrancis@gmail.com>
2019-07-30 02:27:39 -0700

    reconfig webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/react-client/src');
-var DIST_DIR = path.join(__dirname, '/react-client/dist');
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/react-client/src');
+const DIST_DIR = path.join(__dirname, '/react-client/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-       }
-      }
-    ]
-  }
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015'],
+        },
+      },
+    ],
+  },
 };
