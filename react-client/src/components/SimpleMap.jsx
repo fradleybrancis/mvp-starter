62fa62cde8cb458da733df672a22745513f58ffc
fradleybrancis <fradleybrancis@gmail.com>
2019-10-01 17:21:33 -0700

    fix bug in environment variable

--- react-client/src/components/SimpleMap.jsx
+++ react-client/src/components/SimpleMap.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStreetView } from '@fortawesome/free-solid-svg-icons';
-import key from '../../../hideThis.js';
+// import key from '../../../hideThis.js';
 
 const Pin = props => (
   <FontAwesomeIcon icon={faStreetView} />
@@ -24,7 +24,7 @@ class SimpleMap extends Component {
     return (
       <div style={{ height: '450px', width: '50%' }}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key }}
+          bootstrapURLKeys={{ key: process.env.bootstrapURLKeys }}
           defaultCenter={center}
           defaultZoom={zoom}
           onChildClick={this._onChildClick}
